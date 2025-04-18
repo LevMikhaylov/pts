c740b0627994d84ccf3828407f41b3e2b88e654b
LevMikhaylov <152269424+LevMikhaylov@users.noreply.github.com>
2025-04-18 16:08:08 +0300

    Delete demo (3)/demo directory

--- demo (3)/demo/src/main/java/com/example/Repositories/BusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Entities.Bus;
-
-public interface BusRepository extends JpaRepository<Bus,Long>{
-    void findById(long id);
-}
