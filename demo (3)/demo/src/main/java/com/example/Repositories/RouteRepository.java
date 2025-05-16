d2d82061b627e0058899f692ee392ea99711eba2
LevMikhaylov <152269424+LevMikhaylov@users.noreply.github.com>
2025-05-16 15:38:30 +0300

    Delete demo (3)/demo directory

--- demo (3)/demo/src/main/java/com/example/Repositories/RouteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Entities.Route;
-
-
-public interface RouteRepository extends JpaRepository<Route, Long>{
-}
--- demo (3)/demo/src/main/java/com/example/Repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.orders.Entities.Validation.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
